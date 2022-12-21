e2565d4f520e53d15de04066b2e102ccd5c34ab9
saeed <saeed.da1994@gmail.com>
2022-12-21 21:43:16 +0330

    generate collections-home comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ElementsModule} from "./elements/elements.module";
+import {CollectionsModule} from "./collections/collections.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ElementsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
