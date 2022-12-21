d031543920a36a7efc756e3bdab15222a61168b1
saeed <saeed.da1994@gmail.com>
2022-12-21 21:39:08 +0330

    generate element-home comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ElementsModule} from "./elements/elements.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ElementsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
