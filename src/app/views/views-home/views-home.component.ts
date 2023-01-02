8881cd9e26efc1caec741b3674dd744c1165b298
saeed <saeed.da1994@gmail.com>
2023-01-02 21:36:56 +0330

    create a home-view and statistics component

---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.scss']
+})
+export class ViewsHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
