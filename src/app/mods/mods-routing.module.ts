ee9e86833e6ec93a90ab66fe3ef8b714c718ad4f
saeed <saeed.da1994@gmail.com>
2023-01-04 22:16:13 +0330

    add  homeMods comp route

--- src/app/mods/mods-routing.module.ts
+++ src/app/mods/mods-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeModsComponent} from "./home-mods/home-mods.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeModsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
